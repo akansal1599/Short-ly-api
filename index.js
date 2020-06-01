259587dc6f234cc80767e6195b0fc910905524e9
akansal159 <akansal1599@Gmail.com>
2020-06-01 11:50:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
+});
+
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
